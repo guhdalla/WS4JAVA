b1713f722e4a003e2971eb4bee984d0ca25c2106
Gustavo Dalla <61202563+guhdalla@users.noreply.github.com>
2023-04-13 22:31:22 -0300

    Delete src directory

--- src/function/Exerc1.java
+++
@@ -1,8 +0,0 @@
-package function;
-
-public class Exerc1 {
-
-    public static void main(String[] args) {
-        System.out.println("Fun");
-    }
-}
--- src/ifelse/Exerc1.java
+++
@@ -1,8 +0,0 @@
-package ifelse;
-
-public class Exerc1 {
-
-    public static void main(String[] args) {
-        System.out.println("If");
-    }
-}
